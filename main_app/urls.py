a9d0e92ffda936a427eca851bf7dc5287d972f51
Ashley D'Allessandro <ashley.dallessandro@gmail.com>
2021-03-21 19:24:41 -0700

    Production assignment posting

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.index),
+    path("", views.add_anime),
+    path("anime/new", views.newAnime),
+    path("fans", views.add_fans),
+    path("fans/new", views.newFans),
+    path("anime/<int:anime_id>", views.anime_list),
+    path("fans_list/<int:fans_id>", views.fans_list)
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render, redirect
+from .models import Anime, Fans
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def add_anime(request):
+    context = {"all_anime": Anime.objects.all()}
+    return render(request, "add_anime.html", context)
+
+def newAnime(request):
+    print(request.POST)
+    new_anime = Anime.objects.create(
+        title = request.POST["title"],
+        desc = request.POST["desc"]
+    )
+    return redirect("/")
+
+def add_fans(request):
+    context = {"fans": Fans.objects.all()}
+    return render(request, "fans.html", context)
+
+
+def anime_list(request, anime_id):
+    context = {
+        "liked_anime" : Anime.objects.get(id=anime_id),
+        "the_fans" : Fans.objects.all(),
+    }
+    return render(request, "animes.html", context)
+
+def fans_list(request, fans_id):
+    context = {
+        "the_fans" : Fans.objects.get(id=fans_id),
+        "liked_anime" : Anime.objects.all()
+    }
+    return render(request, "fans_list.html", context)
+
+def newFans(request):
+    print(request.POST)
+    new_fan = Fans.objects.create(
+        first_name = request.POST["first_name"],
+        last_name = request.POST["last_name"],
+        notes = request.POST["notes"]
+    )
+    return redirect("/fans")
